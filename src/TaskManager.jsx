f73d78d207e6a3a4e31596c6fb99e4acb95487aa
cshimm <cameronjshimmin@gmail.com>
2024-03-07 11:15:01 -0800

    explanation

--- src/TaskManager.jsx
+++ src/TaskManager.jsx
@@ -13,6 +13,11 @@ export const TaskManager = () => {
         setCount(count => count + 1);
     }
     const toggleTask = (id) => {
+        // Immutability is ensured by mapping over the tasks state and checking if each task item's id
+        //matches the id passed in. If so only that task's completed property is inverted. Here a new task object is also
+        // being created with the properties of the previous task object. Creating a new object then setting that as the
+        //state rather than mutating the state directly ensures state updates are recognized by React so that a render
+        // can be scheduled and performed.
         const updated = tasks.map(task => {
             if (task.id === id)
                 return {
