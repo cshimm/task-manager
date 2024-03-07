eb2da0cc1612bbc8bad5cdfef617d5db2d012c8a
cshimm <cameronjshimmin@gmail.com>
2024-03-06 21:12:57 -0800

    Task manager with React State 2

--- src/TaskManager.jsx
+++ src/TaskManager.jsx
@@ -4,6 +4,7 @@ export const TaskManager = () => {
     const [tasks, setTasks] = useState([])
     const [count, setCount] = useState(0)
     const addTask = () => {
+        console.log(count)
         const newTask = {
             id: count,
             title: `task${count}`,
@@ -12,10 +13,27 @@ export const TaskManager = () => {
         setTasks([...tasks, newTask])
         setCount(n => n + 1)
     }
+    const toggleTask = (e, id) => {
+        e.stopPropagation()
+        console.log(id)
+        const updated = tasks.map(t => {
+            if (t.id === id)
+                return {
+                ...t,
+                    completed: !t.completed
+                }
+            return tasks
+        })
+        setTasks(updated)
+    }
     return (
         <>
             <button onClick={addTask}>add task</button>
-            <p>{tasks.map(task => <li>{task.title}</li>)}</p>
+            <div>{tasks.map(task => {return <div>
+                    <li>{task.title} {task.completed ? "true" : "false"}</li>
+                    <button onClick={(e) => toggleTask(e, task.id)}>Toggle {task.id}</button>
+                </div>
+            })}</div>
         </>
     )
 }
